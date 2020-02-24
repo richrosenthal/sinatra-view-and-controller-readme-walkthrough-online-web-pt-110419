7c76620c627f62e5827cc92fd0b3999307deb008
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-24 17:13:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
-    puts params
+    original_string = params["string"]
+    @reversed_string = orginal_string.reverse 
     erb :reversed
   end
 
